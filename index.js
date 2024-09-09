f491ea85385db325ffc467990ddceb4a48f2cc53
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-09-09 10:08:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require("http"); // Fix the typo here
+
+const fs = require("fs");
+
+const PORT = 4000; // Ensure the port is set properly
+const host = "localhost";
+const home = fs.readFileSync("./indes.html", "utf-8");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  //   res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Type", "text/html"); // Set content type to HTML
+  if (req.url === "/") {
+    // return res.end("<h1>HOME PAGE</h1>");
+    return res.end(home);
+  }
+  if (req.url === "/about") {
+    return res.end("<h1>About PAGE</h1>");
+  }
+  if (req.url === "/contact") {
+    return res.end("<h1>CONTACT PAGE</h1>");
+  }
+  if (req.url === "/service") {
+    return res.end("<h1>SERVICE PAGE</h1>");
+  } else {
+    res.statusCode = 404;
+    return res.end("<h1>ERROR 404 Page not found</h1>");
+  }
+  //   console.log(req.url);
+  //   res.end("<h1>Hello, world!</h1>");
+});
+
+server.listen(PORT, host, () => {
+  console.log(`Server is working on http://${host}:${PORT}`); // Ensure this prints the correct port
+});
